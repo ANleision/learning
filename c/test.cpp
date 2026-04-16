a686b0dbb435270ab6d5836aeeda1454936eb564
ANleision <ANleision@gmail.com>
2026-04-16 23:43:00 +0800

    20260416

---
+++ c/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "你好世界！" << endl;
+    system("pause");    // 防止运行后自动退出，需头文件stdlib.h
+    return 0;
+}
